1bc530da1e4aea13450eaa680e5107dac6d4f8c1
abdurrehmanjami-arj <abdurrehmanjamiwork@gmail.com>
2025-07-14 16:53:20 +0500

    15 quwstions completed

---
+++ app.js
@@ -0,0 +1,156 @@
+// Question No 1:Ask the user for their age. If the age is 18 or more, show an alert: "You are an
+// adult.", else show: "You are a minor."
+
+// var age = parseInt(prompt("Enter You Age"));
+// if (age >= 18) {
+//   alert("You are an Adult");
+// } else {
+//   alert("You are a Minor");
+// }
+
+// Question No 2: Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
+// "Not positive".
+
+// var number = parseInt(prompt("Enter a Number"));
+// if (number >= 0) {
+//   console.log("Positive Number");
+// } else {
+//   console.log("Not Positive");
+// }
+
+// Question No 3: Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd
+// number".
+
+// var number = parseInt(prompt("Enter a Number"));
+// if (number % 2 === 0) {
+//   alert("Even Number");
+// } else {
+//   alert("Odd Number");
+// }
+
+// Question No 4: Ask for a username. If the username is "admin", display "Welcome, admin!"; else
+// display "Access denied."
+
+// var username = prompt("Enter your Username").toLowerCase();
+// if (username === "admin") {
+//   console.log("Welcome, Admin");
+// } else {
+//   console.log("Access Denied");
+// }
+
+// Question No 5: Ask for two numbers. If they are equal, log "Both numbers are the same";
+// otherwise, log "The numbers are different".
+
+// var firstNumber = parseInt(prompt("Enter Your First Number"));
+// var secondNumber = parseInt(prompt("Enter your Second Number"));
+// if (firstNumber === secondNumber) {
+//   console.log("Both Numbers are the Same");
+// } else {
+//   console.log("The Numbers are Different");
+// }
+
+// Question No 6: Ask the user to enter two numbers. Check which one is larger and alert: "First is
+// larger" or "Second is larger".
+
+// var firstNumber = parseInt(prompt("Enter your First Number"));
+// var secondNumber = parseInt(prompt("Enter your Second Number"));
+// if (firstNumber >= secondNumber) {
+//   alert("First is Larger");
+// } else {
+//   alert("Second is lager");
+// }
+
+// Question No 7: Ask for a number. If it's greater than 100, alert "Too big!", else alert "Within
+// range".
+
+// var number = parseInt(prompt("Enter your Number"));
+// if (number > 100) {
+//   alert("Too Big");
+// } else {
+//   alert("Within Range");
+// }
+
+// Question No 8: Ask for a score. If it’s 50 or higher, log "You passed!", else log "You failed."
+
+// var score = parseInt(prompt("Enter your Score"));
+// if (score >= 50) {
+//   console.log("You Passed!");
+// } else {
+//   console.log("You Failed");
+// }
+
+// Question No 9: Ask the user to enter a temperature. If it’s over 30, alert "It's hot!"; else alert "It's
+// not hot."
+
+// var temprature = parseInt(prompt("Enter Temprature"));
+// if (temprature > 30) {
+//   alert("It's Hot!");
+// } else {
+//   alert("It's not Hot");
+// }
+
+// Question No 10: Ask the user to enter a number. If it's 0, alert "Zero"; if it’s not zero, alert "Not zero".
+
+// var number = parseInt(prompt("Enter a Number"));
+// if (number === 0) {
+//   alert("Zero");
+// } else {
+//   alert("Not Zero");
+// }
+
+//Question No 11: Ask the user for their age. If it's 60 or more, log "You are a senior citizen.",
+// else log "You are not a senior."
+
+// var age = parseInt(prompt("Enter your Age"));
+// if (age >= 60) {
+//   console.log("You are a Senior Citizen");
+// } else {
+//   console.log("You are not a Senior");
+// }
+
+// Question No 12: Ask the user for their name. If the name is "Alice", alert "Hi, Alice!"; else alert
+// "You're not Alice!"
+
+// var username = prompt("Enter your Name").toLowerCase();
+// if (username === "alice") {
+//     alert("Hi! Alice")
+// } else {
+//     alert("you are not Alice!")
+// }
+
+// Question No 13: Ask the user for a password. If it matches "12345", log "Correct password"; else
+// log "Wrong password".
+
+// var password = parseInt(prompt("Enter your Password"));
+// if (password === 12345) {
+//   console.log("Correct Passward");
+// } else {
+//   console.log("Wrong Password");
+// }
+
+// Question No 14: Ask the user for the current hour (0–23). If it’s less than 12, alert "Good morning!";
+// else alert "Good afternoon!"
+
+// var currnetHour = parseInt(prompt("Enter your Current Hour. (0 - 23)"));
+// if (currnetHour >= 0 && currnetHour < 12) {
+//   alert("Good Morning");
+// } else if (currnetHour > 23) {
+//   alert("Your Enter Worng Hour");
+// } else {
+//   alert("Good Afternoon!");
+// }
+
+// Question No 15: Ask the user to enter a number. If it's divisible by 5, log "Divisible by 5"; else log
+// "Not divisible by 5".
+
+// var number = parseInt(prompt("Enter a Number.."));
+// if (number % 5 === 0) {
+//   console.log("Divisible by 5");
+// } else {
+//   console.log("Not Divisible by 5");
+// }
+
+// Question No 16: Ask the user if they want to play a game (yes/no). If they type "yes", alert "Let's
+// play!"; else alert "Maybe next time!"
+
+
